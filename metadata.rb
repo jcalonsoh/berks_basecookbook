81be59d84c3a3a145b5b773a04868c8792dadb02
jcalonsoh <j.carlos.alonso.h@gmail.com>
2015-03-26 17:28:42 -0300

    Initializing Project

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'base'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures base'
+long_description 'Installs/Configures base'
+version          '0.1.0'
